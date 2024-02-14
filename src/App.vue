1ab6afbd02e68d098bf2302a93b32c412ed8f17e
Damien <oweb.digital@gmail.com>
2024-02-14 13:14:52 +0100

    Add restaurant card component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="app">
-
+    <RestaurantCard />
   </div>
 </template>
 
 <script>
+import RestaurantCard from './components/RestaurantCard.vue'
 
 export default {
   name: 'App',
+  components: {
+    RestaurantCard
+  }
 }
 </script>
 
@@ -16,10 +20,6 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 #app {
   font-family: 'Poppins', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="restaurant--card">
+    <div class="restaurant--image"></div>
+    <div class="restaurant--information">
+      <div class="top">
+        <p class="name">Subway</p>
+        <p class="note"><span>4.5</span></p>
+      </div>
+      <p class="time"><span>20-30 min</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.restaurant--card {
+  width: 400px;
+  height: 200px;
+  .restaurant--image {
+    background-image: url('./../assets/img/subway.png');
+    height: 100px;
+    background-size: cover;
+    background-position: center;
+  }
+  .restaurant--information {
+    .top {
+      margin: 10px 10px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    .name {
+      font-size: 1.2rem;
+      font-weight: 600;
+    }
+    .note {
+      background-color: #5bee56;
+      height: 30px;
+      width: 30px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 0.8rem;
+      border-radius: 50%;
+    }
+    .time {
+      font-size: 0.9rem;
+      margin: 10px 10px;
+      span {
+        font-weight: 600;
+        background-color:rgba(127, 255, 212, 0.411);
+      }
+    }
+  }
+}
+</style>
